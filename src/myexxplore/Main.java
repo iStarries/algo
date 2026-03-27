03188c87762158f64dddacb4ee2a9e7ea8a80133
iStarries <w18909622321@163.com>
2026-03-27 16:43:35 +0800

    init proj1ect

--- src/myexxplore/Main.java
+++ src/myexxplore/Main.java
@@ -8,51 +8,41 @@ class Main {
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] input = bufferedReader.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
-        int[] arr = new int[n];
+        int m = Integer.parseInt(input[1]);
         input = bufferedReader.readLine().split(" ");
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(input[i]);
-        //trie二维数组的第一维是31 * n，因为每个数字都可能建立一个不一样的二进制路径
-        int[][] bnums = new int[31 * n + 10][2];
-        //idx=0 的时候是根节点
-        int idx = 1;
-        int t = 0;
-        for (int i : arr) {
-            int cur = 0;
-            for (int j = 30; j >= 0 ; j--) {
-//                t = i >> j;
-                //一定要&1，才能取出最后一位
-                t = (i >> j) & 1;
-                if (bnums[cur][t] == 0) {
-                    bnums[cur][t] = idx;
-                    cur = idx++;
-                }
-                else {
-                    cur = bnums[cur][t];
-                }
-            }
-        }
-        int res = 0;
-        for (int i : arr) {
-            int cur = 0;
-            //total实时更新异或结果，最后的total就是一个异或值
-            int total = 0;
-            for (int j = 30; j >= 0 ; j--) {
-                t = (i >> j) & 1;
-                int want = t ^ 1;   // 想走相反位
-                if (bnums[cur][want] != 0) {
-                    total = total * 2 + 1;
-                    cur = bnums[cur][want];
-                }else{
-                    total *= 2;
-                    cur = bnums[cur][t];
-                }
-            }
-            res = Math.max(res, total);
+        int[] arr = new int[n + 10];
+        //下标全部从一开始，因为在树里面要找左右子节点，用2*i 和 2*i+1 会很方便
+        for (int i = 1; i <= n; i++) arr[i] = Integer.parseInt(input[i - 1]);
+        //第1 - n/2个 是非叶子节点，第n/2+1 - n个 是叶子节点
+        //只要从后往前依次对非叶子节点进行下坠就能形成堆
+        for (int i = n / 2; i > 0; i--) down(arr, i, n);
 
+        while (m-- > 0) {
+            //获取、输出对顶
+            //去最后一个节点放到对顶，下坠调整排序
+            bufferedWriter.write(arr[1] + " ");
+            arr[1] = arr[n];
+            n--;
+            down(arr, 1, n);
         }
-        bufferedWriter.write("" + res);
 
         bufferedReader.close();
         bufferedWriter.close();
+    }
+    public static void down(int[] arr, int i, int n) {
+        int j = i;
+        //判断左孩子存不存在，取最小值的索引
+        //判断右孩子存不存在，取最小值的索引
+        //把取出来的最小值的索引和原本传进来的索引进行值交换
+        if(i * 2 <= n && arr[i * 2] < arr[j]) j = i * 2;
+        if(i * 2 + 1 <= n && arr[i * 2 + 1] < arr[j]) j = i * 2 + 1;
+        //现在j对应的值就是三者里面的最小值
+
+        if (i != j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            down(arr, j, n);
+        }
     }
 }
