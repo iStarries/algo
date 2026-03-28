772da962217b2c0e5853c172b3ab18e2d30eefa6
iStarries <w18909622321@163.com>
2026-03-28 13:17:59 +0800

    init proj1ect

--- src/myexxplore/Main.java
+++ src/myexxplore/Main.java
@@ -3,46 +3,62 @@ package myexxplore;
 import java.io.*;
 
 class Main {
+    public static int N = 100003;
+    /*
+    数组模拟单链表所使用的三个数组
+    head存所有的头节点，这里也是哈希表的所有桶
+    val存所有节点的值
+    next存所有节点的下一个节点
+     */
+    public static int[] head = new int[N];
+    public static int[] val = new int[N];
+    public static int[] next = new int[N];
+    public static int cur = 1;
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] input = bufferedReader.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
-        input = bufferedReader.readLine().split(" ");
-        int[] arr = new int[n + 10];
-        //下标全部从一开始，因为在树里面要找左右子节点，用2*i 和 2*i+1 会很方便
-        for (int i = 1; i <= n; i++) arr[i] = Integer.parseInt(input[i - 1]);
-        //第1 - n/2个 是非叶子节点，第n/2+1 - n个 是叶子节点
-        //只要从后往前依次对非叶子节点进行下坠就能形成堆
-        for (int i = n / 2; i > 0; i--) down(arr, i, n);
 
-        while (m-- > 0) {
-            //获取、输出对顶
-            //去最后一个节点放到对顶，下坠调整排序
-            bufferedWriter.write(arr[1] + " ");
-            arr[1] = arr[n];
-            n--;
-            down(arr, 1, n);
+        while (n-- > 0) {
+            input = bufferedReader.readLine().split(" ");
+            char ch = input[0].charAt(0);
+            int x = Integer.parseInt(input[1]);
+            if (ch == 'Q') {
+                if (query(x)) System.out.println("Yes");
+                else System.out.println("No");
+            }
+            else insert(x);
         }
+
 
         bufferedReader.close();
         bufferedWriter.close();
     }
-    public static void down(int[] arr, int i, int n) {
-        int j = i;
-        //判断左孩子存不存在，取最小值的索引
-        //判断右孩子存不存在，取最小值的索引
-        //把取出来的最小值的索引和原本传进来的索引进行值交换
-        if(i * 2 <= n && arr[i * 2] < arr[j]) j = i * 2;
-        if(i * 2 + 1 <= n && arr[i * 2 + 1] < arr[j]) j = i * 2 + 1;
-        //现在j对应的值就是三者里面的最小值
 
-        if (i != j){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            down(arr, j, n);
+    public static boolean query(int x) {
+        //这里的哈希函数避免了负数k
+        int k = (x % N + N) % N;
+        if (head[k] == 0){
+            return false;
         }
+        k = head[k];
+        while (k != 0) {
+            if (val[k] == x) {
+                return true;
+            }
+            k = next[k];
+        }
+        return false;
     }
+
+    public static void insert(int x) {
+        if (query(x)) return;
+        int k = (x % N + N) % N;
+        val[cur] = x;
+        next[cur] = head[k];
+        head[k] = cur;
+        cur++;
+    }
+
 }
